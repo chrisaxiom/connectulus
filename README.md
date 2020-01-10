0a3526db38a5ac1cea4c9e8a68b75f966ad0ac0b
Topher Marie <topherMarie@gmail.com>
2020-01-10 15:16:09 -0400

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# evaluator
+# Evaluator
+## Purpose
+Package auth provides (mock) implementations of functions useful for
+connecting users to a remote application.
+
+## Contents
--- auth.go
+++ auth.go
@@ -1,3 +1,6 @@
+/*
+	Package auth provides (mock) implementations of functions useful for connecting users to a remote application.
+*/
 package auth
 
 import (
@@ -5,13 +8,18 @@ import (
 	"time"
 )
 
+// RemoteConnection is a struct that represents a network connection to remote application
 type RemoteConnection struct {
 }
 
+// ValidatePassword takes a username and a password as arguments, returning a true if
+// the password is valid, and false otherwise
 func ValidatePassword(u, p string) bool {
 	return u == p
 }
 
+// ConnectRemote establishes a network connection to a remote application and returns a
+// RemoteConnection that can be used for communication
 func ConnectRemote() *RemoteConnection {
 	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 	return &RemoteConnection{}
